20ab4989de6ed9100fbc3dc0ef3b1474055fdfdf
IanC <chenxian352@gmail.com>
2019-09-21 01:24:00 +1200

    test

--- functions/chrome/chrome.js
+++ functions/chrome/chrome.js
@@ -24,7 +24,7 @@ exports.handler = async (event, context, callback) => {
       waitUntil: ["domcontentloaded", "networkidle0"]
     })
 
-    await page.waitForSelector('#phenomic')
+    await page.waitForSelector('.Sidebar--about--2lYWu')
 
     theTitle = await page.title();
 
