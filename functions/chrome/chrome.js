f27c1dc951b2c243f57e0a936666a2497da4479c
IanC <chenxian352@gmail.com>
2019-09-23 18:15:56 +1200

    testing graphcms

--- functions/chrome/chrome.js
+++ functions/chrome/chrome.js
@@ -44,7 +44,7 @@ exports.handler = async (event, context, callback) => {
 
     await fetch('https://api-apeast.graphcms.com/v1/ck0vzjke6268t01d490ee68nw/master', {
       method: 'POST',
-      body: {
+      body: JSON.stringify({
         query: `
         mutation {
           updateDomHtml(
@@ -56,7 +56,7 @@ exports.handler = async (event, context, callback) => {
           }
         }
         `
-      }
+      })
     })
     .then(response => response.json())
     .then(data => (
