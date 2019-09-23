479b13558360ea3c346741a94c220ff802ce0286
IanC <chenxian352@gmail.com>
2019-09-23 18:48:05 +1200

    testing graphcms

--- functions/chrome/chrome.js
+++ functions/chrome/chrome.js
@@ -8,83 +8,118 @@ exports.handler = async (event, context, callback) => {
   let targetDOM = null
   const targetSelector = '.pools__items-wrapper'
 
-  console.log('Spawning Chrome Headless')
-
-  try {
-    const executablePath = await chromium.executablePath
-
-    // setup
-    browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: executablePath,
-      headless: chromium.headless,
+  let getCachedDOMHTML = false;
+  console.log('Fetching GraphCMS')
+  await fetch("https://api-apeast.graphcms.com/v1/ck0vzjke6268t01d490ee68nw/master", {
+    method: 'POST',
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          domHtml(where: {id: "ck0vzt2ns1hq60830o1rk92fu"}) {
+            jsNow
+            id
+            body
+          }
+        }
+        `
     })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if ( (data.data.jsNow + 600000) > Date.now() ) {
+      console.log('Cached GraphCMS is updated in 10 minutes', data)
+      getCachedDOMHTML = true
+      targetDOM = data.data.body
+    }
+  })
 
-    // Do stuff with headless chrome
-    const page = await browser.newPage()
-    const targetUrl = 'https://www.rugbyworldcup.com/'
+  if (!getCachedDOMHTML) {
 
-    // Goto page and then do stuff
-    await page.goto(targetUrl, {
-      waitUntil: ["domcontentloaded", "networkidle0"]
-    })
-    // await page.waitFor(3000)
-    await page.waitForSelector(targetSelector)
+    console.log('Spawning Chrome Headless')
 
-    theTitle = await page.title();
-    targetDOM = await page.$eval(targetSelector, e => e.outerHTML);
-    targetDOM = targetDOM.replace("\n", '').
-      replace(/\\n/, '').
-      replace(/\n/, '');
+    try {
+      const executablePath = await chromium.executablePath
 
-    console.log('Page Loaded: ', theTitle)
-    console.log('Target DOM HTML: ', targetDOM.substr(0, 100))
+      // setup
+      browser = await puppeteer.launch({
+        args: chromium.args,
+        executablePath: executablePath,
+        headless: chromium.headless,
+      })
 
-    console.log('Updating GraphCMS.')
+      // Do stuff with headless chrome
+      const page = await browser.newPage()
+      const targetUrl = 'https://www.rugbyworldcup.com/'
 
-    await fetch("https://api-apeast.graphcms.com/v1/ck0vzjke6268t01d490ee68nw/master", {
-      method: 'POST',
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify({
-        query: `
+      // Goto page and then do stuff
+      await page.goto(targetUrl, {
+        waitUntil: ["domcontentloaded", "networkidle0"]
+      })
+      // await page.waitFor(3000)
+      await page.waitForSelector(targetSelector)
+
+      theTitle = await page.title();
+      targetDOM = await page.$eval(targetSelector, e => e.outerHTML);
+      targetDOM = targetDOM.replace("\n", '').
+        replace(/\\n/, '').
+        replace(/\n/, '');
+      targetDOM = JSON.stringify(targetDOM)
+      console.log('Page Loaded: ', theTitle)
+      console.log('Target DOM HTML: ', targetDOM.substr(0, 100))
+
+      console.log('Updating GraphCMS.')
+
+      let jsNow = Date.now()
+      await fetch("https://api-apeast.graphcms.com/v1/ck0vzjke6268t01d490ee68nw/master", {
+        method: 'POST',
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+          query: `
           mutation {
             updateDomHtml(
               where: { id: "ck0vzt2ns1hq60830o1rk92fu" }
-              data: { body: ${targetDOM} }
+              data: { 
+                body: ${targetDOM}
+                jsNow: ${jsNow} 
+              }
             ) {
               id
               body
             }
           }
         `
+        })
       })
-    })
-    .then(response => response.json())
-    .then(data => (
-      console.log('Updated GraphCMS', data)
-    ))
+      .then(response => response.json())
+      .then(data => (
+        console.log('Updated GraphCMS', data)
+      ))
 
-  } catch (error) {
-    console.log('error', error)
-    return callback(null, {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: error
+    } catch (error) {
+      console.log('error', error)
+      return callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          error: error
+        })
       })
-    })
-  } finally {
-    // close browser
-    if (browser !== null) {
-      await browser.close()
+    } finally {
+      // close browser
+      if (browser !== null) {
+        await browser.close()
+      }
     }
   }
+
 
   return callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      title: theTitle,
       targetDOM: targetDOM
     })
   })
